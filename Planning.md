0b1ae98e4d883549984e92e98a9c423a68c13920
parthi-n <parthiban.n@live.com>
2024-10-24 18:38:36 +0800

    added controllers

---
+++ Planning.md
@@ -0,0 +1,27 @@
+# Planning
+
+
+## Minimum Viable Product (MVP)
+- Able to move the snake (steered left, right, up, and down)
+- Able to increase the snake length when shake eats the food
+- Game ends when the head collides its body 
+
+
+## Userstory
+
+- As a user, I want to move the snake using keyboard arrow keys and navigate accross the screen.
+- As a user, I want visual feedback when snake eats the bait.
+- As a user, I want visual feedback when snake collide with itself.
+- As a user, I want to set boundary for the play area
+- As a user, i want to record the score (X eaten by snake)
+
+## MVC 
+
+Model, View, Controller
+
+Model -> render() -> DOM
+
+Event Listener -> modify the model + render()
+
+
+ ???
--- main.js
+++ main.js
@@ -1,24 +1,84 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/*-------------------------------- Constants --------------------------------*/
+const rightBtnEl = document.getElementById("right");
+const leftBtnEl = document.getElementById("left");
+const upBtnEl = document.getElementById("up");
+const downBtnEl = document.getElementById("down");
+const startBtnEl = document.getElementById("start");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+console.log(upBtnEl);
 
-setupCounter(document.querySelector('#counter'))
+const startPoint = document.getElementById("1-1");
+console.log(startPoint);
+
+/*---------- Variables (state) ---------*/
+
+// Snake - array with object
+let snake = [{ x: 1, y: 1 }];
+
+// Direction
+//let direction = right;
+
+// Food postion
+let food = { x: 2, y: 2 };
+
+// Score
+// let score = 0
+
+// Gameover
+// let gameOver = false
+
+/*-------------- Functions -------------*/
+
+const rightBtnClick = () => {
+	console.log("clicked");
+};
+
+const leftBtnClick = () => {
+	console.log("clicked");
+};
+const upBtnClick = () => {
+	console.log("clicked");
+};
+const downBtnClick = () => {
+	console.log("clicked");
+};
+
+const startBtnClick = () => {
+	console.log("Game started");
+	startPoint.classList.add("snake");
+};
+
+// start game
+const startGame = () => {
+	// set snake position
+
+	console.log("test");
+	// Food position randomly
+	// Initialize game variables and start loop
+};
+
+startGame();
+
+// Update Position
+// ???
+
+// GrowSnake
+
+// check collions + end game
+
+// check food eaten + add score
+
+// generate food at position
+
+// Set timer for food postion?
+
+// Clear game area
+
+/*----------- Event Listeners ----------*/
+
+// Direction based on key input
+rightBtnEl.addEventListener("click", rightBtnClick);
+leftBtnEl.addEventListener("click", leftBtnClick);
+upBtnEl.addEventListener("click", upBtnClick);
+downBtnEl.addEventListener("click", downBtnClick);
+startBtnEl.addEventListener("click", startBtnClick);
